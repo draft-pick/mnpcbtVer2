ee72500c1e2de2117146d29e8204baa66c5b2ea7
draftpick <draft-pick@yandex.ru>
2020-10-26 22:15:51 +0300

    macbook

--- news/views.py
+++ news/views.py
@@ -17,4 +17,11 @@ def view_news(request, news_id):
     news_item = News.objects.get(pk=news_id)
     image_title = GalleryNews.objects.all().filter(in_the_title=1, product_id=news_id)
     image = GalleryNews.objects.all().filter(product_id=news_id)
-    return render(request, 'news/view_news.html', {"news_item": news_item, 'image': image, 'image_title': image_title})
+    context = {
+        'title': news_item.title,
+        "news_item": news_item,
+        'image': image,
+        'image_title': image_title
+    }
+    return render(request, 'news/view_news.html', context=context)
+
