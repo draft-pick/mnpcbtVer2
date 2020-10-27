1775d95ed91db621cdb087a09c2ad8b47d7deaf0
draftpick <draft-pick@yandex.ru>
2020-10-27 15:39:22 +0300

    imac21

--- mnpcbtVer2/settings.py
+++ mnpcbtVer2/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'allstatic')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
